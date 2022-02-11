fc4ce74890b99fcf2a6bc0a42e3e9cc193a3c595
Jesus Lencina <jesusignaciolencina@gmail.com>
2022-02-11 12:25:21 -0300

    first commit - starting out

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <section className="hero">
+        <div className="hero-text">
+          <h1 className="text-primary">
+            Recuperando la filatelia por internet
+          </h1>
+          <p>
+            Quizás las cartas hayan quedado en el pasado, pero nuestras
+            estampillas merecen ser recordadas.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
