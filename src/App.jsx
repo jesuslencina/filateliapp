ffb3783a2db174cf3694140821af6a96a255e9ee
Jesus Lencina <jesusignaciolencina@gmail.com>
2022-02-11 12:53:00 -0300

    styling setup done + finished main text layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import "./App.css";
+import "./App.scss";
+import Img from "react-cool-img";
+
+import logo from "./assets/logo-huge.png";
 
 const App = () => {
   return (
     <div className="App">
-      <section className="hero">
+      <section className="hero spaced">
+        <div className="logo">
+          <Img src={logo} alt="Logo Filateliapp" width="28" height="31" />
+          <p>FILATELIAPP</p>
+        </div>
         <div className="hero-text">
           <h1 className="text-primary">
             Recuperando la filatelia por internet
